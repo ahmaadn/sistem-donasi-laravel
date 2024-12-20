e7a0667b07e6c95adbfba1cb0278685cc73db671
Ahmad Nur <ahmadnursahid12345@gmail.com>
2024-12-20 21:49:23 +0700

    feat: add migrations to create new tables

--- database/migrations/2024_12_20_144405_create_donations_table.php
+++ database/migrations/2024_12_20_144405_create_donations_table.php
@@ -10,12 +10,12 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('donations', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->enum('role', ['user', 'admin'])->default('user');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('campaign_id')->constrained('campaigns')->onDelete('cascade');
+            $table->decimal('amount', 15, 2);
+            $table->string('message')->nullable();
             $table->timestamps();
         });
     }
@@ -25,6 +25,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('donations');
     }
 };
