d8be41898403f499d0f17de01f99ff7c6e7eda91
Ahmad Nur <ahmadnursahid12345@gmail.com>
2024-12-28 00:08:06 +0700

    feat: update footer and header with new contact information and branding

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -9,45 +9,34 @@
                 <h5 class="site-footer-title mb-3">Quick Links</h5>
 
                 <ul class="footer-menu">
-                    <li class="footer-menu-item"><a href="#" class="footer-menu-link">Our Story</a></li>
-
-                    <li class="footer-menu-item"><a href="#" class="footer-menu-link">Newsroom</a></li>
-
                     <li class="footer-menu-item"><a href="#" class="footer-menu-link">Causes</a></li>
-
-                    <li class="footer-menu-item"><a href="#" class="footer-menu-link">Become a
-                            volunteer</a></li>
-
-                    <li class="footer-menu-item"><a href="#" class="footer-menu-link">Partner with us</a>
-                    </li>
+                    <li class="footer-menu-item"><a href="#" class="footer-menu-link">Donasi</a></li>
                 </ul>
             </div>
 
             <div class="col-lg-4 col-md-6 col-12 mx-auto">
-                <h5 class="site-footer-title mb-3">Contact Infomation</h5>
+                <h5 class="site-footer-title mb-3">Kontak Informasi</h5>
 
                 <p class="text-white d-flex mb-2">
                     <i class="bi-telephone me-2"></i>
 
                     <a href="tel: 305-240-9671" class="site-footer-link">
-                        305-240-9671
+                        089630xxxx
                     </a>
                 </p>
 
                 <p class="text-white d-flex">
                     <i class="bi-envelope me-2"></i>
 
-                    <a href="mailto:info@yourgmail.com" class="site-footer-link">
-                        donate@charity.org
+                    <a href="mailto:2206042@itg.ac.id" class="site-footer-link">
+                        2206042@itg.ac.id
                     </a>
                 </p>
 
                 <p class="text-white d-flex mt-3">
                     <i class="bi-geo-alt me-2"></i>
-                    Akershusstranda 20, 0150 Oslo, Norway
+                    Jl. Mayor Syamsu No.1, Jayaraga, Kec. Tarogong Kidul, Kabupaten Garut, Jawa Barat 44151
                 </p>
-
-                <a href="#" class="custom-btn btn mt-3">Get Direction</a>
             </div>
         </div>
     </div>
@@ -57,13 +46,12 @@
             <div class="row">
 
                 <div class="col-lg-6 col-md-7 col-12">
-                    <p class="copyright-text mb-0">Copyright © 2036 <a href="#">Kind Heart</a> Charity Org.
-                        Design: <a href="https://templatemo.com" target="_blank">TemplateMo</a><br>Distribution:
-                        <a href="https://themewagon.com">ThemeWagon</a>
+                    <p class="copyright-text mb-0">
+                        Copyright © 2026 <a href="{{ route('home') }}">Kita Donasi</a>
                     </p>
                 </div>
 
-                <div class="col-lg-6 col-md-5 col-12 d-flex justify-content-center align-items-center mx-auto">
+                {{-- <div class="col-lg-6 col-md-5 col-12 d-flex justify-content-center align-items-center mx-auto">
                     <ul class="social-icon">
                         <li class="social-icon-item">
                             <a href="#" class="social-icon-link bi-twitter"></a>
@@ -85,8 +73,7 @@
                             <a href="https://youtube.com/templatemo" class="social-icon-link bi-youtube"></a>
                         </li>
                     </ul>
-                </div>
-
+                </div> --}}
             </div>
         </div>
     </div>
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -5,19 +5,19 @@
             <div class="col-lg-8 col-12 d-flex flex-wrap">
                 <p class="d-flex me-4 mb-0">
                     <i class="bi-geo-alt me-2"></i>
-                    Akershusstranda 20, 0150 Oslo, Norway
+                    Jl. Mayor Syamsu No.1, Jayaraga, Kec. Tarogong Kidul, Kabupaten Garut, Jawa Barat 44151
                 </p>
 
                 <p class="d-flex mb-0">
                     <i class="bi-envelope me-2"></i>
 
-                    <a href="mailto:info@company.com">
-                        info@company.com
+                    <a href="mailto:2206042@itg.ac.id">
+                        2206042@itg.ac.id
                     </a>
                 </p>
             </div>
 
-            <div class="col-lg-3 col-12 ms-auto d-lg-block d-none">
+            {{-- <div class="col-lg-3 col-12 ms-auto d-lg-block d-none">
                 <ul class="social-icon">
                     <li class="social-icon-item">
                         <a href="#" class="social-icon-link bi-twitter"></a>
@@ -39,7 +39,7 @@
                         <a href="#" class="social-icon-link bi-whatsapp"></a>
                     </li>
                 </ul>
-            </div>
+            </div> --}}
 
         </div>
     </div>
@@ -47,10 +47,10 @@
 
 <nav class="navbar navbar-expand-lg bg-light shadow-lg">
     <div class="container">
-        <a class="navbar-brand" href="index.html">
+        <a class="navbar-brand" href="{{ route('home') }}">
             <img src="images/logo.png" class="logo img-fluid" alt="Kind Heart Charity">
             <span>
-                Kind Heart Charity
+                Kita Donasi
                 <small>Non-profit Organization</small>
             </span>
         </a>
@@ -67,26 +67,11 @@
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link click-scroll" href="#section_2">About</a>
-                </li>
-
-                <li class="nav-item">
                     <a class="nav-link click-scroll" href="#section_3">Causes</a>
                 </li>
 
                 <li class="nav-item">
-                    <a class="nav-link click-scroll" href="#section_4">Volunteer</a>
-                </li>
-
-                <li class="nav-item dropdown">
-                    <a class="nav-link click-scroll dropdown-toggle" href="#section_5" id="navbarLightDropdownMenuLink"
-                        role="button" data-bs-toggle="dropdown" aria-expanded="false">News</a>
-
-                    <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarLightDropdownMenuLink">
-                        <li><a class="dropdown-item" href="news.html">News Listing</a></li>
-
-                        <li><a class="dropdown-item" href="news-detail.html">News Detail</a></li>
-                    </ul>
+                    <a class="nav-link click-scroll" href="#section_2">About</a>
                 </li>
 
                 <li class="nav-item">
